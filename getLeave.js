8eecadd3326206abf7f2683aac04d3e82c6fef9c
Xuekai <66772155+xuekaitt@users.noreply.github.com>
2022-05-07 18:19:36 +0800

    Update getLeave.js

--- getLeave.js
+++ getLeave.js
@@ -57,7 +57,7 @@ var user = {
         {
         "reason" : "",
         "position" : "电子信息学院",
-        "time" : "05.06 20:41",
+        "time" : "05.06 20:31",
         "head" : "",
         "userType" : 4,
         "name" : "",
@@ -72,7 +72,7 @@ var user = {
         "name" : "",
         "state" : 2
         }
-    ],
+    ]
 };
 
 Date.prototype.format=function(fmt){var o={"M+":this.getMonth()+1,"d+":this.getDate(),"h+":this.getHours(),"m+":this.getMinutes(),"s+":this.getSeconds(),"q+":Math.floor((this.getMonth()+3)/3),"S":this.getMilliseconds()};if(/(y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length))};for(var k in o){if(new RegExp("("+k+")").test(fmt)){fmt=fmt.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)))}};return fmt};
